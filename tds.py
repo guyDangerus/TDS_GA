2738e27b318df6816736f6a682bc855ba9e63c67
Ayushi Patel <121036083+A0129s@users.noreply.github.com>
2024-04-21 03:26:11 +0530

    Update tds.py

--- tds.py
+++ tds.py
@@ -9,9 +9,9 @@ def largest(a,b,c):
 
 st.title("Largest in three numbers")
 
-x = st.number_input("Enter first number:")
-y = st.number_input("Enter second number:")
-z = st.number_input("Enter third number:")
+x = st.number_input("Enter first number:", step = 0.0001, value = 0.0000, format = "%5.4f")
+y = st.number_input("Enter second number:" step = 0.0001, value = 0.0000, format = "%5.4f")
+z = st.number_input("Enter third number:" step = 0.0001, value = 0.0000, format = "%5.4f")
 
 st.text("The largest among these three numbers:")
 st.write(largest(x,y,z))
